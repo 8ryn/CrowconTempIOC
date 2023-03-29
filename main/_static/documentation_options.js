11f2bac837b0daf6c38cfa535d64f41ff20e3019
8ryn <8ryn@users.noreply.github.com>
2023-03-29 11:15:13 +0000

    deploy: 45a008848a74433b33e2d6fb1958b5f8df9f5e78

--- main/_static/documentation_options.js
+++ main/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.1.dev57+g9edfcf7',
+    VERSION: '0.1.dev59+g45a0088',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
